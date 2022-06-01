f70037df5240983d120660ffa4864a49bf100058
Wollivan <tim.smith2814@gmail.com>
2022-06-01 10:10:21 +0100

    Made changes together

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,17 +1,65 @@
-let Hour = [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+const hour = [
+  "6am",
+  "7am",
+  "8am",
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+  "6pm",
+  "7pm",
+];
 
-let Seattle = {
-    customerRange: [23,65],
-    averageConversionRate_cookieValue: 6.3,
-    estimatedCustomer_perHour: function() {
-        return Math.floor(Math.random() * (this.customerRange[1] - this.customerRange[0]) + this.customerRange[0]);
-    },
-    cookiesPurchased_oneHour: function() {
-        return Math.floor(this.averageConversionRate_cookieValue * this.estimatedCustomer_perHour())
-    },
-    changeCusRange: function(min, max) {
-        return this.customerRange = [min, max];
-    },
+let seattle = {
+  customerRange: [23, 65],
+  averageConversionRate_cookieValue: 6.3,
+  customersEachHour: [],
+  cookiesSoldEachHour: [],
+  totalDailyCookies: 0,
+  estimatedCustomer_perHour: function () {
+    for (let a = 0; a < hour.length; a++) {
+      this.customersEachHour.push(
+        random(this.customerRange[0], this.customerRange[1])
+      );
+    }
+  },
+  cookiesPurchased_oneHour: function () {
+    for (let a = 0; a < this.customersEachHour.length; a++) {
+      this.cookiesSoldEachHour.push(
+        Math.floor(
+          this.customersEachHour[a] * this.averageConversionRate_cookieValue
+        )
+      );
+    }
+  },
+  changeCusRange: function (min, max) {
+    return (this.customerRange = [min, max]);
+  },
+  render() {
+    this.estimatedCustomer_perHour();
+    this.cookiesPurchased_oneHour();
+    const unorderedList = document.getElementById("seattle");
+    for (let a = 0; a < this.customersEachHour.length; a++) {
+      const listItem = document.createElement("li");
+      listItem.textContent = `${hour[a]}: ${this.customersEachHour[a]} cookies`;
+      unorderedList.appendChild(listItem);
+      this.totalDailyCookies =
+        this.totalDailyCookies + this.customersEachHour[a];
+    }
+    const listItem = document.createElement("li");
+    listItem.textContent = `Total: ${this.totalDailyCookies} cookies`;
+    unorderedList.appendChild(listItem);
+  },
 };
 
-console.log(Seattle.cookiesPurchased_oneHour())
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+seattle.render();
+console.log(seattle);
